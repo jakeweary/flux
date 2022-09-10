9ec8b15e8810c6a44c2de4565d75add602f6784c
me <literally@me>
2022-09-10 16:18:26 +0300

    refactor simplex3d

--- src/glsl/lib/simplex3d.glsl
+++ src/glsl/lib/simplex3d.glsl
@@ -1,20 +1,16 @@
 // https://www.shadertoy.com/view/XsX3zB
 
 // discontinuous pseudorandom uniformly distributed in [-0.5, +0.5]^3
-vec3 random3(vec3 c) {
+vec3 simplex3d_random3(vec3 c) {
   float j = 4096.0 * sin(dot(c, vec3(17.0, 59.4, 15.0)));
-  vec3 r;
-  r.z = fract(512.0 * j); j *= .125;
-  r.x = fract(512.0 * j); j *= .125;
-  r.y = fract(512.0 * j);
-  return r - 0.5;
+  return fract(j * vec3(64.0, 8.0, 512.0)) - 0.5;
 }
 
 // 3d simplex noise
 float simplex3d(vec3 p) {
   // skew constants
-  const float F3 = 0.3333333;
-  const float G3 = 0.1666667;
+  const float F3 = 1.0 / 3.0;
+  const float G3 = 1.0 / 6.0;
 
   // 1. find current tetrahedron T and it's four vertices
   // s, s+i1, s+i2, s+1.0 - absolute skewed (integer) coordinates of T vertices
@@ -35,22 +31,25 @@ float simplex3d(vec3 p) {
   vec3 x3 = x - 1.0 + 3.0 * G3;
 
   // 2. find four surflets and store them in d
-  vec4 w, d;
 
   // calculate surflet weights
-  w.x = dot(x, x);
-  w.y = dot(x1, x1);
-  w.z = dot(x2, x2);
-  w.w = dot(x3, x3);
+  vec4 w = vec4(
+    dot(x, x),
+    dot(x1, x1),
+    dot(x2, x2),
+    dot(x3, x3)
+  );
 
   // w fades from 0.6 at the center of the surflet to 0.0 at the margin
   w = max(0.6 - w, 0.0);
 
   // calculate surflet components
-  d.x = dot(random3(s), x);
-  d.y = dot(random3(s + i1), x1);
-  d.z = dot(random3(s + i2), x2);
-  d.w = dot(random3(s + 1.0), x3);
+  vec4 d = vec4(
+    dot(simplex3d_random3(s), x),
+    dot(simplex3d_random3(s + i1), x1),
+    dot(simplex3d_random3(s + i2), x2),
+    dot(simplex3d_random3(s + 1.0), x3)
+  );
 
   // multiply d by w^4
   w *= w;
@@ -64,11 +63,13 @@ float simplex3d(vec3 p) {
 // directional artifacts can be reduced by rotating each octave
 float simplex3d_fractal(vec3 m) {
   // const matrices for 3d rotation
-  const mat3 rot1 = mat3(-0.37, 0.36, 0.85, -0.14, -0.93, 0.34, 0.92, 0.01, 0.4);
-  const mat3 rot2 = mat3(-0.55, -0.39, 0.74, 0.33, -0.91, -0.24, 0.77, 0.12, 0.63);
-  const mat3 rot3 = mat3(-0.71, 0.52, -0.47, -0.08, -0.72, -0.68, -0.7, -0.45, 0.56);
-  return 0.5333333 * simplex3d(1.0 * m * rot1)
-       + 0.2666667 * simplex3d(2.0 * m * rot2)
-       + 0.1333333 * simplex3d(4.0 * m * rot3)
-       + 0.0666667 * simplex3d(8.0 * m);
+  const mat3 rot1 = mat3(-0.37, +0.36, +0.85, -0.14, -0.93, +0.34, +0.92, +0.01, +0.40);
+  const mat3 rot2 = mat3(-0.55, -0.39, +0.74, +0.33, -0.91, -0.24, +0.77, +0.12, +0.63);
+  const mat3 rot3 = mat3(-0.71, +0.52, -0.47, -0.08, -0.72, -0.68, -0.70, -0.45, +0.56);
+  return dot(vec4(8.0, 4.0, 2.0, 1.0) / 15.0, vec4(
+    simplex3d(1.0 * m * rot1),
+    simplex3d(2.0 * m * rot2),
+    simplex3d(4.0 * m * rot3),
+    simplex3d(8.0 * m)
+  ));
 }
